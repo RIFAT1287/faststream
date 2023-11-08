cafad4b743ee3ef350f145fb28464995862532b8
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2023-11-08 12:37:23 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,15 +1,15 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
-    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    API_ID = int(env.get("TELEGRAM_API_ID", 11421014))
+    API_HASH = env.get("TELEGRAM_API_HASH", "9ac3fd02dccbde3435d6b58a5e430bcb")
+    OWNER_ID = int(env.get("OWNER_ID", 5076258969))
+    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "5076258969").split()
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "AnimeLoungeLeechbot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6594858799:AAH6eW59XyW2UpDORCD6f-Lo9FrRKU1PkgY")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001830345770))
     BOT_WORKERS = int(env.get("BOT_WORKERS", 10))
-    SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
+    SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 3))
 
 class Server:
     BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
